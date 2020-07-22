0a641099279a6333716b0acf7d8470c8bfc11f23
Jaladh Singhal <jaladhsinghal@gmail.com>
2020-07-22 17:51:53 +0530

    Added leap week functionality

--- life-matrix.js
+++ life-matrix.js
@@ -1,17 +1,33 @@
 const matrixContainer = document.querySelector("#matrix");
 
-const expectedAge = 100;
+const LIFE_EXPECTANCY = 100;
+const WEEKS_IN_YEAR = 52;
+const DAYS_IN_WEEK = 7;
 
 function genMatrix(dob) {
   matrixContainer.innerHTML = "";
-  let row;
-  for (let i = 0; i <= expectedAge; i++) {
-    // TODO: Find right way to declare row
+  let row,
+    daysInAgeYear,
+    remDaysOverAgeYears = 0;
+  for (let ageYear = 0; ageYear <= LIFE_EXPECTANCY; ageYear++) {
     row = document.createElement("div");
     row.classList.add("row");
 
+    // Find week remainder days in age year
+    daysInAgeYear = getDaysInAgeYear(ageYear, dob);
+    remDaysOverAgeYears += daysInAgeYear % DAYS_IN_WEEK;
+
+    // Add em in a var to determine if it's 7 -> leap week
+    if (remDaysOverAgeYears >= 7) {
+      remDaysOverAgeYears -= 7;
+      weeksInAgeYear = WEEKS_IN_YEAR + 1;
+      row.classList.add("row--leap-week");
+    } else {
+      weeksInAgeYear = WEEKS_IN_YEAR;
+    }
+
     let box;
-    for (let j = 1; j <= 52; j++) {
+    for (let week = 1; week <= weeksInAgeYear; week++) {
       box = document.createElement("div");
       box.classList.add("box");
       row.appendChild(box);
@@ -22,6 +38,25 @@ function genMatrix(dob) {
 
   alert("you entered: " + dob);
   //TODO: Fill boxes based on passed dob string
+}
+
+function getDaysInAgeYear(ageYear, dobStr) {
+  const MS_PER_DAY = 1000 * 60 * 60 * 24;
+  const dob = new Date(dobStr);
+
+  // Discard the time and time-zone information.
+  const curBday = Date.UTC(
+    dob.getFullYear() + ageYear,
+    dob.getMonth(),
+    dob.getDate()
+  );
+  const nextBday = Date.UTC(
+    dob.getFullYear() + ageYear + 1,
+    dob.getMonth(),
+    dob.getDate()
+  );
+
+  return Math.floor((nextBday - curBday) / MS_PER_DAY);
 }
 
 // TODO: Populate matrix div with a grid
